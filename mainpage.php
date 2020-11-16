c453a866f77e1bffe60a0d71b01a44ad8d29b04f
MMMurieLLL <qianxunxun@outlook.com>
2020-11-16 08:42:07 -0800

    add main page

---
+++ mainpage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Main Page for Delivery Database</title>
+  </head>
+  <body>
+  <p class="title">CPSC 304 Delivery Database</p>
+  <p>Proceed as User:</p>
+  <input name="newPage" type="button" value="Cutomer" onclick="window.open('customer.php')">
+  <input name="newPage" type="button" value="Courier" onclick="window.open('courier.php')">
+  <input name="newPage" type="button" value="Restaurant Staff" onclick="window.open('restaurant.php')">
+  <p>Proceed as Delivery App Staff:</p>
+  <input name="newPage" type="button" value="Open Delivery App Database" onclick="window.open('delivery.php')"/>
+  </body>
+</html>
