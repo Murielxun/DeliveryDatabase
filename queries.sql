868a351089fc72ddf75abe5fab6b0f3b35d2c0e4
Levarza <39745992+Levarza@users.noreply.github.com>
2020-11-17 01:00:44 -0800

    Update queries.sql
    Modified some queries.

--- queries.sql
+++ queries.sql
@@ -1,75 +1,107 @@
---hardcoded queries
 
--- projection query
-SELECT date_placed, payment_method
-FROM Order
 
--- aggregation with group by
--- find max rating of restaurants, where restaurants are grouped by category
-SELECT category, MAX(rating)
-FROM Restaurant 
-GROUP BY category
+-- 1) Insertion Query: Create a customer account
+-- Inputs to be provided by user: customer_id, email, age, phone_number, street_address, postal_code, name
+INSERT INTO Customer 
+    VALUES(customer_id, email, age, phone_number, street_address, postal_code, name, 0, 'Bronze', 0.0);
 
--- aggregation with having 
--- find all customers who have ordered at least three orders
-SELECT customer_id, COUNT(*)
-FROM Order
-GROUP BY customer_id
-HAVING COUNT(*) > 2
+-- 2) Deletion Query: Deleting a customer account
+-- Inputs to be provided by user: delete_cid
+DELETE FROM Customer 
+WHERE customer_id = delete_cid;
 
--- nested aggregation with with group by 
--- find those categories for which their avg price is the min over all categories
-SELECT temp.category, temp.avgprice
-FROM (SELECT item.category, AVG(item.price) avgprice
-      FROM Menu_Items_Has item
-      GROUP BY item.category) AS temp
-WHERE temp.avgprice = (SELECT MIN (temp.avgprice)
-                       FROM temp)
-
--- division 
--- find the customer who have ordered all restaurant
-SELECT Customer.name
-FROM Customer
-WHERE NOT EXISTS (SELECT restaurant_id
-                  FROM Restaurant
-                  WHERE NOT EXISTS (SELECT restaurant_id
-                                    FROM Order
-                                    WHERE Order.restaurant_id = Restaurant.restaurant_id AND Order.customer_id = Customer.customer_id))
-                                    
- -- Insertion Query
--- Create a customer account
-INSERT INTO Customer VALUES(customer_id, email, age, phone_number, street_address, postal_code,
-	                        name, reward_points, rewards_tier, account_balance);
-
--- Deletion Query
--- Delete a customer account
--- (delete_cid is to be provided by the user)
-DELETE FROM Customer WHERE customer_id = delete_cid;
-
--- Update a customer's information
--- (updated_email and u_cid are to be provided by the user)
+-- 3) Update Query: Update a customer's email
+-- Inputs to be provided by user: new_email, u_cid
 UPDATE Customer
-SET email = updated_email
-WHERE customer_id = u_cid; 
+SET email = new_email
+WHERE customer_id = u_cid;
 
--- Selection Query
--- Find all restaurants that have a rating greater than a specified number,
+-- 4) Update Query: Update a customer's age
+-- Inputs to be provided by user: new_age, u_cid
+UPDATE Customer
+SET age = new_age
+WHERE customer_id = u_cid;
+
+-- 5) Update Query: Update a customer's phone number
+-- Inputs to be provided by user: new_number, u_cid
+UPDATE Customer
+SET phone_number = new_number
+WHERE customer_id = u_cid;
+
+-- 6) Update Query: Update a customer's street address and postal code
+-- Inputs to be provided by user: new_addr, new_postalcode, u_cid
+UPDATE Customer
+SET street_address = new_addr,
+    postal_code = new_postalcode
+WHERE customer_id = u_cid;
+
+-- 7) Selection Query: Find all restaurants that have a rating greater than a specified number,
 -- where 0.0 is the minimum rating and 10.0 is the maximum rating.
--- (num is to be provided by the user)
+-- Inputs to be provided by the user: val
 SELECT restaurant_id, name
 FROM Restaurant
-WHERE rating > num;
+WHERE rating > val;
 
--- Join Query
--- Join the Order, Customer, and Restaurant tables to find information about the 
--- spending habits and restaurant preferences of customers of a specific rewards tier.
--- (specify_tier is to be provided by the user)
-SELECT order_number, food_subtotal, customer_id, restaurant_id, rating
-FROM Customer JOIN Orders
-    ON Customer.customer_id = Orders.customer_id
-    JOIN Restaurant
+-- 8) Projection Query: Find the date placed, food subtotal, and payment method for all orders.
+SELECT date_placed, food_subtotal, payment_method
+FROM Orders;
+
+-- 9) Join Query: For all restaurants that have a rating that is higher than some value, return the average food subtotal for 
+-- the orders made at each of those restaurants, as well as the IDs, names, and ratings of the restaurants.
+-- Inputs to be provided by the user: val
+SELECT avg(food_subtotal), restaurant_id, name, rating
+FROM Restaurant JOIN Orders
     ON Restaurant.restaurant_id = Orders.restaurant_id
-WHERE rewards_tier = 'specify_tier';
+WHERE rating > val
+GROUP BY restaurant_id;
+
+-- 10) Aggregation with Group By Query: For each restaurant category, find the restaurants that have the highest rating in that category. 
+SELECT name, category, MAX(rating)
+FROM Restaurant 
+GROUP BY category;
+
+-- 11) Aggregation with Having Query: Find the name and age of customers who have placed at least three orders, where only
+-- orders with a food subtotal > 50 are considered. 
+SELECT DISTINCT C.name, C.age
+FROM Orders O, Customer C
+WHERE O.customer_id = C.customer_id AND O.food_subtotal > 50
+GROUP BY customer_id
+HAVING COUNT(*) > 2;
+
+-- 12) Nested Aggregation with Group By Query: Find the restaurant categories that have the lowest average price of menu items  
+-- compared to all other restaurant categories.  
+SELECT category, avgprice
+FROM Restaurant R, Menu_Items_Has M
+WHERE R.restaurant_id = M.restaurant_id
+GROUP BY R.category 
+HAVING AVG(M.price) <= ALL (SELECT AVG(M2.price)
+                            FROM Restaurant R2, Menu_Items_Has M2
+                            WHERE R2.restaurant_id = M2.restaurant_id
+                            GROUP BY R2.category);
+
+-- 13) Division Query: Find all customers who have ordered from all restaurants in Vancouver, BC.
+SELECT DISTINCT C.name
+FROM Customer C
+WHERE NOT EXISTS (SELECT R.restaurant_id
+                  FROM Restaurant R, Address A
+                  WHERE R.postal_code = A.postal_code 
+                    AND A.city = 'Vancouver' 
+                    AND A.province = 'BC'
+                    EXCEPT (SELECT O.restaurant_id
+                            FROM Orders O, Restaurant R2, Address A2
+                            WHERE O.restaurant_id = R2.restaurant_id 
+                                AND R2.postal_code = A2.postal_code
+                                AND A2.city = 'Vancouver'
+                                AND A2.province = 'BC'
+                                AND O.customer_id = C.customer_id));
+                                    
+
+
+
+
+
+
+
 
 
 
