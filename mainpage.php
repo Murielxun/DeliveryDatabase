c4b137d4720fcd62d1f20bdb2a26b722635c89e1
UBC Student <vicche04@valdes.students.cs.ubc.ca>
2020-11-21 04:20:12 -0800

    vic

--- mainpage.php
+++ mainpage.php
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
 <html>
+<link rel="stylesheet" href="style.css">
+<div class = "restaurant">
   <head>
     <title>Main Page for Delivery Database</title>
   </head>
-  <body>
-  <p class="title">CPSC 304 Delivery Database</p>
+</div>
+  <body class = "courierBody" >
+  <div class = "restaurant">
+      <h1>CPSC 304 Group Project - Delivery Database</h1>
+      <h3>Group Members: Victor Cheng, Jenni Park, Muriel Wang</h3>
+      <hr />
+  </div>
+
+  <div class = "restaurant">
   <p>Proceed as User:</p>
   <input name="newPage" type="button" value="Cutomer" onclick="window.open('customer.php')">
   <input name="newPage" type="button" value="Courier" onclick="window.open('courier.php')">
   <input name="newPage" type="button" value="Restaurant Staff" onclick="window.open('restaurant.php')">
   <p>Hard-coded Queries for Project:</p>
-  <input name="newPage" type="button" value="Check Them Out" onclick="window.open('304project.php')"/>
+  <input name="newPage" type="button" value="Check Them Out" onclick="window.open('hardcode.php')"/>
+
+  <button onclick="getLocation()">GPS</button>
+  <div id="mapholder"></div>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBEfiGdCFozkk3ippbqmzcsrl-6tewyRM&callback=initMap"
+          type="text/javascript"></script>
+  </div>
   </body>
+<script src = "GPS.js"></script>
 </html>
